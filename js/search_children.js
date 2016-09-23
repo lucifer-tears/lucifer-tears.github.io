02076a2cc9c11aaf9c5a2b7561c5155def02e59e
lucifer-tears <lucifel_tears@163.com>
2016-09-23 11:42:21 +0800

    Site updated: 2016-09-23 11:42:20

---
+++ js/search_children.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-09-23 11:38:31
+ * @version $Id$
+ */
+var _base = 'http://qzone.qq.com/gy/404/';
+document.write('<script type="text/javascript" src="' + _base + 'data.js" charset="utf-8"></script>');
+document.write('<script type="text/javascript" src="'+ _base + 'page.js" charset="utf-8"></script>');/*  |xGv00|bb048611f4d0bb34934fd40353d94edf */
